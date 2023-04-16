52ff209b6c645135b37272de2eb8539b668d531b
KimYeonWook <kimyeonwook511@gmail.com>
2023-04-16 21:19:16 +0900

    chore : 기본 설정
    메이븐(pom.xml) 라이브러리 의존 설정 추가 (MySQL-Connector, Mybatis, spring-test)
    web.xml 한글 인코딩 설정
    home.jsp -> main.jsp로 파일명 변경
    HomeController 테스트

--- src/main/java/com/studywithme/main/HomeController.java
+++ src/main/java/com/studywithme/main/HomeController.java
@@ -1,9 +1,5 @@
 package com.studywithme.main;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -11,29 +7,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String rootGET(Model model) {
+		logger.info("SWMProject Start");
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return "main";
 	}
 	
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String mainGET(Model model) {
+		logger.info("SWMProject Start");
+		
+		return "main";
+	}
+	
+	@RequestMapping(value = "/t1", method = RequestMethod.GET)
+	public String t1GET(Model model) {
+		logger.info("SWMProject Start");
+		
+		return "main";
+	}
 }
