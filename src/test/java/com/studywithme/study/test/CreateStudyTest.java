cde7301485a980ecf24fcfde96eba77a64eb4ad8
KimYeonWook <kimyeonwook511@gmail.com>
2023-05-01 16:46:44 +0900

    test: 스터디 공고 등록 및 조회 테스트

---
+++ src/test/java/com/studywithme/study/test/CreateStudyTest.java
@@ -0,0 +1,33 @@
+package com.studywithme.study.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.studywithme.domain.StudyDTO;
+import com.studywithme.persistence.StudyDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class CreateStudyTest {
+	
+	@Inject
+	private StudyDAO dao;
+	
+	@Test
+	public void createStudyTest() {
+		try {
+			StudyDTO dto = new StudyDTO();
+			dto.setStudyTitle("테스트1");
+			dto.setStudyContent("테스트1내용");
+			dto.setStudyWriter("테스트작성자");
+			dao.createStudy(dto);
+			
+		} catch (Exception e) {
+			System.out.println("실패");
+		}
+	}
+}
