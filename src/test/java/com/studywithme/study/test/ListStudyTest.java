aa73f4fa70b63647088c48e5491ea3c53507585f
KimYeonWook <kimyeonwook511@gmail.com>
2023-05-08 15:20:54 +0900

    test: 스터디 공고 리스트 조회 테스트

---
+++ src/test/java/com/studywithme/study/test/ListStudyTest.java
@@ -0,0 +1,28 @@
+package com.studywithme.study.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.studywithme.persistence.StudyDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class ListStudyTest {
+	
+	@Inject
+	private StudyDAO dao;
+	
+	@Test
+	public void listStudyTest() {
+		try {
+			System.out.println(dao.listStudy());
+			
+		} catch (Exception e) {
+			System.out.println("½ÇÆÐ");
+		}
+	}
+}
