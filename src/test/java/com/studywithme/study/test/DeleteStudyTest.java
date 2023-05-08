996b45960635619c73967b3b54051601476cfcc7
KimYeonWook <kimyeonwook511@gmail.com>
2023-05-08 15:20:28 +0900

    test: 스터디 공고 삭제 테스트

---
+++ src/test/java/com/studywithme/study/test/DeleteStudyTest.java
@@ -0,0 +1,28 @@
+package com.studywithme.study.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.studywithme.persistence.StudyDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DeleteStudyTest {
+	
+	@Inject
+	private StudyDAO dao;
+	
+	@Test
+	public void deleteStudyTest() {
+		try {
+			dao.deleteStudy(5);
+			
+		} catch (Exception e) {
+			System.out.println("½ÇÆÐ");
+		}
+	}
+}
