18b79685aab8b232d57ba67d379cef01f82f1046
KimYeonWook <kimyeonwook511@gmail.com>
2023-05-29 00:05:48 +0900

    test: 스터디 지원서 합격/불합격 기능 테스트

---
+++ src/test/java/com/studywithme/study/test/ApplyFailTest.java
@@ -0,0 +1,28 @@
+package com.studywithme.study.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.studywithme.persistence.StudyDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class ApplyFailTest {
+	
+	@Inject
+	private StudyDAO dao;
+	
+	@Test
+	public void applyCheckTest() {
+		try {
+			dao.applyFail(1);
+			
+		} catch (Exception e) {
+			System.out.println("½ÇÆÐ");
+		}
+	}
+}
