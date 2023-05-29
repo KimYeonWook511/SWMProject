00a0fb9f4c86bdc067d898cd0397b5077ae35e41
KimYeonWook <kimyeonwook511@gmail.com>
2023-05-29 22:48:41 +0900

    test: 스터디 공고 삭제시 지원한 지원서 삭제 기능 테스트

---
+++ src/test/java/com/studywithme/study/test/DeleteStudyApplyTest.java
@@ -0,0 +1,29 @@
+package com.studywithme.study.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.studywithme.persistence.StudyDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DeleteStudyApplyTest {
+	
+	@Inject
+	private StudyDAO dao;
+	
+	@Test
+	public void deleteStudyApplyTest() {
+		try {
+			dao.deleteStudy(8);
+			dao.deleteStudyApply(8);
+			
+		} catch (Exception e) {
+			System.out.println("½ÇÆÐ");
+		}
+	}
+}
