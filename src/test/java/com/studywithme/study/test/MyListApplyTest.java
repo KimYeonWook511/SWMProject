500e358b9fca032b8dbda563dc0a29afe4e3bdca
KimYeonWook <kimyeonwook511@gmail.com>
2023-05-29 21:54:59 +0900

    test: 나의 지원서 리스트 기능 테스트

---
+++ src/test/java/com/studywithme/study/test/MyListApplyTest.java
@@ -0,0 +1,28 @@
+package com.studywithme.study.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.studywithme.persistence.StudyDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class MyListApplyTest {
+	
+	@Inject
+	private StudyDAO dao;
+	
+	@Test
+	public void myListApplyTest() {
+		try {
+			System.out.println(dao.myListApply("test"));
+			
+		} catch (Exception e) {
+			System.out.println("½ÇÆÐ");
+		}
+	}
+}
