0210fb0710d454747790c9a8a5be60825ec59c7b
KimYeonWook <kimyeonwook511@gmail.com>
2023-06-19 20:55:07 +0900

    test: 나의 스터디 그룹 리스트 조회 기능 테스트 수정
    사용하지 않는 GroupDTO import 제거

--- src/test/java/com/studywithme/group/test/MyGroupListTest.java
+++ src/test/java/com/studywithme/group/test/MyGroupListTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.studywithme.domain.GroupDTO;
 import com.studywithme.domain.GroupVO;
 import com.studywithme.persistence.GroupDAO;
 
